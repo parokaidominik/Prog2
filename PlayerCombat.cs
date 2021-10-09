4b27743ea76cd3310c5ab801aacc41be1d2ef7a9
parokaidominik <78852330+parokaidominik@users.noreply.github.com>
2021-10-09 17:29:33 +0200

    Add files via upload
    Week1 & Week2 progress

---
+++ PlayerCombat.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCombat : MonoBehaviour
+{
+    public Animator anim;
+
+    public Transform attackPoint;
+    public LayerMask enemyLayers;
+
+    public float AttackRange = 0.5f;
+    public int attackDamage = 40;
+
+    public float attackRate = 2f;
+    private float nextAttackTime = 0f;
+
+    void Update()
+    {
+        if(Time.time >= nextAttackTime){
+            if(Input.GetKeyDown(KeyCode.Space)){
+            Attack();
+            nextAttackTime = Time.time + 1f / attackRate;
+
+        }
+        }
+        
+        
+    }
+
+    void Attack(){
+
+        //Attack animáció
+        anim.SetTrigger("Attack");
+
+        //Enemy érzékelése rangen belül
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, AttackRange, enemyLayers);
+
+        //Damage
+        foreach(Collider2D enemy in hitEnemies){
+            
+            enemy.GetComponent<Enemy>().TakeDamage(attackDamage);
+        }
+
+    }
+
+    void OnDrawGizmosSelected(){
+
+        if (attackPoint == null)
+        return;
+        Gizmos.DrawWireSphere(attackPoint.position, AttackRange);
+    }
+}
+
---
+++ PlayerMovement.cs
@@ -0,0 +1,55 @@
+
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    private Rigidbody2D body;
+    private Animator anim;
+    private bool grounded;
+
+    private void Awake(){
+        body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+
+    }
+
+    private void Update(){
+
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        body.velocity = new Vector2(horizontalInput*speed,body.velocity.y);
+
+
+        //Merre néz
+        if(horizontalInput > 0.01f)
+        transform.localScale = Vector3.one;
+        else if(horizontalInput < -0.01f)
+        transform.localScale = new Vector3(-1,1,1);
+
+        if(Input.GetKey(KeyCode.W) && grounded)
+            Jump();
+
+
+        //Animációk
+
+        anim.SetBool("Run", horizontalInput != 0);
+        anim.SetBool("grounded",grounded);
+            }
+
+
+        private void Jump(){
+            body.velocity =new Vector2(body.velocity.x, 6);
+            anim.SetTrigger("jump");
+            grounded = false;
+           
+        }
+
+        private void OnCollisionEnter2D(Collision2D collision){
+            if(collision.gameObject.tag == "Ground")
+            grounded = true;
+
+        }
+
+
+}
